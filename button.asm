dce84e5226bf83e95becee16150bbad0bd1cdae8
peachyKeachy <156981435+peachyKeachy@users.noreply.github.com>
2024-04-28 18:01:56 +0000

    init

--- button.asm
+++ button.asm
@@ -3,7 +3,7 @@
 .def led = r21
 
 .org OVF1addr                         ; Timer overflow
-    jmp tm1_ISR
+    jmp tm1_ISR 
 
 tm1_init:
 ; initialize timer1 with interrupts
@@ -23,3 +23,18 @@ tm1_init:
     sts TIMSK1,r20
 
     ret
+
+; void gpioInit()
+; ------------------------------------------------------------
+gpioInit:
+; set input button
+	      cbi       DDRD,ButtonPin      ; set Blue LED Btn to input (D2)
+          sbi       PORTD,ButtonPin     ; engage pull-up
+          sbi       EIMSK,INT0          ; enable external interrupt 0 for Blue LED Btn
+          ldi       r20,0b00000010      ; set falling edge sense bits for ext int 0
+          sts       EICRA,r20
+
+          ret       
+
+
+
