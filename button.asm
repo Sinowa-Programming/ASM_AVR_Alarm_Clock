b942682bebae6a3a8039cbacb068506dfd899429
SiangCh <67034864+Sinowa-Programming@users.noreply.github.com>
2024-04-28 14:56:03 -0400

    Update button.asm

--- button.asm
+++ button.asm
@@ -1,9 +1,11 @@
+.equ interruptPin = D2
+
 ; void gpioInit()
 ; ------------------------------------------------------------
 gpioInit:
 ; set input button
-	      cbi       DDRD,ButtonPin      ; set Blue LED Btn to input (D2)
-          sbi       PORTD,ButtonPin     ; engage pull-up
+	      cbi       DDRD,interruptPin      ; set interruptPin to input (D2)
+          sbi       PORTD,interruptPin     ; engage pull-up
           sbi       EIMSK,INT0          ; enable external interrupt 0 for Blue LED Btn
           ldi       r20,0b00000010      ; set falling edge sense bits for ext int 0
           sts       EICRA,r20
